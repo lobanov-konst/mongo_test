b2664434a1e0c60de423388753f89707a0abdf7c
konst <lobanov.konst@gmail.com>
2015-06-26 16:20:13 +0300

    Added clear old data.

--- libs/measure.js
+++ libs/measure.js
@@ -21,29 +21,30 @@ function run(timeout) {
     var _date = new Date();
     _date.setDate(_date.getDate() - 1);
     mongoose.model('measure').remove({"time": {"$lt": _date}}, function (err, res) {
-    });
 
-    var memUsage = os.freemem() / os.totalmem() * 100,
-        cpuUsage = null,
-        cpu = os.cpus(),
-        cpu1 = {
-            'user': getCPUUsage('user', cpu),
-            'sys': getCPUUsage('sys', cpu),
-            'idle': getCPUUsage('idle', cpu)
-        },
-        cpu2 = null;
-    setTimeout(function () {
-        cpu = os.cpus();
-        cpu2 = {
-            'user': getCPUUsage('user', cpu),
-            'sys': getCPUUsage('sys', cpu),
-            'idle': getCPUUsage('idle', cpu)
-        };
-        cpuUsage = (cpu2.user - cpu1.user + cpu2.sys - cpu1.sys) * 100 / (cpu2.user - cpu1.user + cpu2.sys - cpu1.sys + cpu2.idle - cpu1.idle);
-        data = new measureModel({time: new Date(), cpu: cpuUsage, mem: memUsage});
-        data.save();
-        setTimeout(run, timeout || 1000);
-    }, 1000);
+        var memUsage = os.freemem() / os.totalmem() * 100,
+            cpuUsage = null,
+            cpu = os.cpus(),
+            cpu1 = {
+                'user': getCPUUsage('user', cpu),
+                'sys': getCPUUsage('sys', cpu),
+                'idle': getCPUUsage('idle', cpu)
+            },
+            cpu2 = null;
+        setTimeout(function () {
+            cpu = os.cpus();
+            cpu2 = {
+                'user': getCPUUsage('user', cpu),
+                'sys': getCPUUsage('sys', cpu),
+                'idle': getCPUUsage('idle', cpu)
+            };
+            cpuUsage = (cpu2.user - cpu1.user + cpu2.sys - cpu1.sys) * 100 / (cpu2.user - cpu1.user + cpu2.sys - cpu1.sys + cpu2.idle - cpu1.idle);
+            data = new measureModel({time: new Date(), cpu: cpuUsage, mem: memUsage});
+            data.save();
+            setTimeout(run, timeout || 1000);
+        }, 1000);
+
+    });
 }
 
 
